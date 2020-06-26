406b9412053762bd8ddeab978610581ca8d80ba7
Roberto Henríquez Perozo <rihp94@gmail.com>
2020-06-26 15:26:26 +0200

    signup form color

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,7 +25,7 @@
             />
 
             <button
-              class="flex-shrink-0 bg-blue-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
+              class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
             >
               Sign Up
