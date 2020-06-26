66e9a0fb846fa2f8a1eb730e26b9aa7990a0b4a1
Roberto Henríquez Perozo <rihp94@gmail.com>
2020-06-26 15:53:08 +0200

    changing nav baro test

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -2,7 +2,7 @@
   <header class="flex items-center py-4 md:py-8">
     <div class="header__logo">
       <nuxt-link to="/">
-        <img :src="logo" alt="Logo" />
+        <img :src="logo" alt="Logo" /> <h1>dEngine</h1>
       </nuxt-link>
     </div>
 
